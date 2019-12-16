829485b9e8ce0738b1a6dda868c15e3b2296bfbf
xiaofei <alibaba22001@163.com>
2019-12-16 13:04:31 +0800

    update

--- gen_video_thumbnail.sh
+++ gen_video_thumbnail.sh
@@ -6,21 +6,26 @@
 #   文件名称：gen_video_thumbnail.sh
 #   创 建 者：肖飞
 #   创建日期：2019年12月12日 星期四 12时37分42秒
-#   修改日期：2019年12月12日 星期四 12时39分58秒
+#   修改日期：2019年12月16日 星期一 13时03分49秒
 #   描    述：
 #
 #================================================================
 function main() {
-	cat log | while read line; do
+	cat only_filelist | while read line; do
 		if [ ! -f "$line" ]; then
 			continue;
 		fi
 
-		if [ -f "$line.jpg" ]; then
+		local tn_path="thumbnails/$line.jpg"
+
+		if [ -f "$tn_path" ]; then
 			continue;
 		fi
 
-		ffmpeg -nostdin -ss "0:00:50" -i "$line" -an -vframes 1 "$line.jpg";
+		local tn_dir=$(dirname "$tn_path")
+		mkdir -p "$tn_dir"
+
+		ffmpeg -nostdin -ss "0:00:50" -i "$line" -an -vframes 1 "$tn_path";
 	done
 }
 
