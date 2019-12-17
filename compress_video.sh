885b514a44ee4c4990d664106b55b8c206b77466
xiaofei <alibaba22001@163.com>
2019-12-17 14:58:15 +0800

    update

---
+++ compress_video.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#================================================================
+#   
+#   
+#   文件名称：compress_video.sh
+#   创 建 者：肖飞
+#   创建日期：2019年12月17日 星期二 14时44分06秒
+#   修改日期：2019年12月17日 星期二 14时50分31秒
+#   描    述：
+#
+#================================================================
+function main() {
+	local video_path="$1"
+	local new_video_path="$1.compressed.mp4"
+
+	if [ ! -f "$video_path" ]; then
+		return;
+	fi
+
+	#ffmpeg -i "$video_path" -c:v libx264 -crf 23 -preset veryslow -tune film -c:a aac -strict -2 -b:a 128k "$new_video_path";
+	#ffmpeg -i "$video_path" -c:v libx264 -crf 23 -preset veryslow -tune animation -c:a aac -strict -2 -b:a 128k "$new_video_path";
+	ffmpeg -nostdin -i "$video_path" -c:v libx264 -crf 23 -preset veryslow -tune film -c:a aac -strict -2 -b:a 128k "$new_video_path";
+}
+
+main $@
