c1315b6f97dc937123e14832f8fd883edff82f75
xiaofei <alibaba22001@163.com>
2019-12-17 17:33:54 +0800

    update

---
+++ cleanup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#================================================================
+#   
+#   
+#   文件名称：cleanup.sh
+#   创 建 者：肖飞
+#   创建日期：2019年12月17日 星期二 17时21分47秒
+#   修改日期：2019年12月17日 星期二 17时33分15秒
+#   描    述：
+#
+#================================================================
+function main() {
+	find gifs -type f | while read line;do
+		local videofile=$(echo "$line" | sed 's/^gifs\///g' | sed 's/\.gif$//g')
+		if [ -f "$videofile" ]; then
+			continue;
+		fi
+		echo "not exist $videofile"
+		rm "$line"
+	done
+
+	find thumbnails -type f | while read line;do
+		local videofile=$(echo "$line" | sed 's/^thumbnails\///g' | sed 's/\.jpg$//g')
+		if [ -f "$videofile" ]; then
+			continue;
+		fi
+		echo "not exist $videofile"
+		rm "$line"
+	done
+}
+
+main $@
