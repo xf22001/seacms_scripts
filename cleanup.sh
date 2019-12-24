e05142c18192536983e516efa0fe0ee238ca84d6
xiaofei <alibaba22001@163.com>
2019-12-24 11:06:28 +0800

    update

--- cleanup.sh
+++ cleanup.sh
@@ -6,13 +6,13 @@
 #   文件名称：cleanup.sh
 #   创 建 者：肖飞
 #   创建日期：2019年12月17日 星期二 17时21分47秒
-#   修改日期：2019年12月17日 星期二 17时33分15秒
+#   修改日期：2019年12月24日 星期二 11时05分55秒
 #   描    述：
 #
 #================================================================
 function main() {
 	find gifs -type f | while read line;do
-		local videofile=$(echo "$line" | sed 's/^gifs\///g' | sed 's/\.gif$//g')
+		local videofile="$(echo "$line" | sed 's/^gifs\///g' | sed 's/\.gif$//g')"
 		if [ -f "$videofile" ]; then
 			continue;
 		fi
@@ -21,7 +21,7 @@ function main() {
 	done
 
 	find thumbnails -type f | while read line;do
-		local videofile=$(echo "$line" | sed 's/^thumbnails\///g' | sed 's/\.jpg$//g')
+		local videofile="$(echo "$line" | sed 's/^thumbnails\///g' | sed 's/\.jpg$//g')"
 		if [ -f "$videofile" ]; then
 			continue;
 		fi
