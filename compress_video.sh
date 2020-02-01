23dab2f5a2b5e7c8bd43d584067d96a226f3454a
xiaofei <alibaba22001@163.com>
2020-02-01 11:40:28 +0800

    update

--- compress_video.sh
+++ compress_video.sh
@@ -6,7 +6,7 @@
 #   文件名称：compress_video.sh
 #   创 建 者：肖飞
 #   创建日期：2019年12月17日 星期二 14时44分06秒
-#   修改日期：2019年12月17日 星期二 14时50分31秒
+#   修改日期：2020年02月01日 星期六 11时40分09秒
 #   描    述：
 #
 #================================================================
@@ -20,7 +20,7 @@ function main() {
 
 	#ffmpeg -i "$video_path" -c:v libx264 -crf 23 -preset veryslow -tune film -c:a aac -strict -2 -b:a 128k "$new_video_path";
 	#ffmpeg -i "$video_path" -c:v libx264 -crf 23 -preset veryslow -tune animation -c:a aac -strict -2 -b:a 128k "$new_video_path";
-	ffmpeg -nostdin -i "$video_path" -c:v libx264 -crf 23 -preset veryslow -tune film -c:a aac -strict -2 -b:a 128k "$new_video_path";
+	ffmpeg -threads 4 -nostdin -i "$video_path" -c:v libx264 -crf 20 -preset veryslow -tune film -c:a aac -strict -2 -b:a 128k "$new_video_path";
 }
 
 main $@
