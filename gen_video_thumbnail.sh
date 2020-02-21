b313712088c590263fb3c34bcc915b5fddf6f8ec
xiaofei <alibaba22001@163.com>
2020-02-21 18:54:58 +0800

    update

--- gen_video_thumbnail.sh
+++ gen_video_thumbnail.sh
@@ -6,7 +6,7 @@
 #   文件名称：gen_video_thumbnail.sh
 #   创 建 者：肖飞
 #   创建日期：2019年12月12日 星期四 12时37分42秒
-#   修改日期：2019年12月16日 星期一 13时03分49秒
+#   修改日期：2020年02月21日 星期五 18时54分34秒
 #   描    述：
 #
 #================================================================
@@ -25,6 +25,7 @@ function main() {
 		local tn_dir=$(dirname "$tn_path")
 		mkdir -p "$tn_dir"
 
+		echo ffmpeg -nostdin -ss "0:00:50" -i "$line" -an -vframes 1 "$tn_path";
 		ffmpeg -nostdin -ss "0:00:50" -i "$line" -an -vframes 1 "$tn_path";
 	done
 }
