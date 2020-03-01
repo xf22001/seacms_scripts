87a1d62373ef24dd0e7939190f2e0fb5ac514d01
xiaofei <alibaba22001@163.com>
2020-03-01 14:23:56 +0800

    update

--- gen_video_filelist.sh
+++ gen_video_filelist.sh
@@ -6,12 +6,12 @@
 #   文件名称：gen_video_filelist.sh
 #   创 建 者：肖飞
 #   创建日期：2019年12月12日 星期四 12时40分50秒
-#   修改日期：2019年12月16日 星期一 12时34分27秒
+#   修改日期：2020年03月01日 星期日 14时23分03秒
 #   描    述：
 #
 #================================================================
 function main() {
-	find . -type f | grep -i "\.avi$\|\.mp4$\|\.wmv$" | while read line; do
+	find . -type f | grep -i "\.avi$\|\.mp4$\|\.wmv$\|\.rm$\|\.rmvb$" | while read line; do
 		du -sh "$line";
 	done | sort -h | uniq > size_filelist
 
